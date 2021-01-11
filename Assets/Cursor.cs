351c47fd15ec517dc2c64909f70da496424e69b1
Dion Pimentel <dionpimentel260@gmail.com>
2021-01-11 16:56:46 -0700

    cursor changes
    - added basic sprites for cursor

---
+++ Assets/Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+        
+
+        //transform.position = Input.mousePosition;
+        //print(Input.mousePosition);
+    }
+}
