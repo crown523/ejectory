fc8c1261c6b672946526e4f1e769db3e6f6c15f8
Dion Pimentel <dionpimentel260@gmail.com>
2021-01-11 23:59:54 -0700

    added switching between sprites, but no mechanics

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Cursor : MonoBehaviour
 {
     private Vector3 mousePosition;
+    private Animator animator;
     public float moveSpeed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +20,14 @@ public class Cursor : MonoBehaviour
         mousePosition=Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
 
-        
+        if(Input.GetMouseButtonDown(1) && !animator.GetBool("swap"))
+        {
+            animator.SetBool("swap", true);
+        }
+        else if(Input.GetMouseButtonDown(0) && animator.GetBool("swap"))
+        {
+            animator.SetBool("swap", false);
+        }
 
         //transform.position = Input.mousePosition;
         //print(Input.mousePosition);
