b024f5ff5350f9f5c92da1159f3c844c6026d995
crown523 <kxlider@rocketmail.com>
2021-01-12 21:20:56 +0800

    implement locking

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Animator animator;
+    public float moveSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+        if(Input.GetMouseButtonDown(1) && !animator.GetBool("swap")) 
+        // can we rename swap to isLockMode or something similar
+        // so that its more clear which mode it's in
+        {
+            animator.SetBool("swap", true);
+        }
+        else if(Input.GetMouseButtonDown(1) && animator.GetBool("swap"))
+        {
+            animator.SetBool("swap", false);
+        }
+
+        //transform.position = Input.mousePosition;
+        //print(Input.mousePosition);
+
+        // handle locking
+        if (Input.GetMouseButtonDown(0) && !animator.GetBool("swap")) 
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null) 
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                // if we clicked a lockable object
+                if (hit.collider.gameObject.GetComponent<Lockable>())
+                {
+                    // get ref to the script
+                    Lockable objScript = hit.collider.gameObject.GetComponent<Lockable>();
+                    // delegate behavior to the object
+                    objScript.Lock();
+                }
+                //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Lockable.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lockable : MonoBehaviour
+{
+    //private bool moving;
+    private Rigidbody2D body;
+    private bool locked;
+    private float timeWhenLocked;
+    private Vector3 newVelocity;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //moving = false;
+
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector3(0.0f, speed, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(!moving && Input.GetKeyDown("space"))
+        {
+            
+        }
+        */
+
+        //print(body.velocity.y);
+
+        if (Time.time - timeWhenLocked > 3) // locks last 3 seconds
+        {
+            locked = false;
+            body.velocity = newVelocity;
+            body.velocity = new Vector3(2, 2, 0); // for debug
+        }
+
+    }
+
+    public void Lock()
+    {
+        if (!locked)
+        {
+            locked = true;
+            timeWhenLocked = Time.time;
+            print("locked");
+            body.velocity = new Vector3(0,0,0); // kill movement
+        }
+        
+    }
+
+    public void ApplyMomentum()
+    {
+        if (locked)
+        {
+            print("touched");
+            // update newVelocity here
+        }
+        
+    }
+}
