1bd0e5c190a2cf124517cdaa1de38dade2df4a8c
crown523 <kxlider@rocketmail.com>
2021-01-18 20:37:22 +0800

    implement level select algorithm, automatically loads correct scene with one function

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectManager : MonoBehaviour
 {
@@ -20,6 +21,13 @@ public class LevelSelectManager : MonoBehaviour
     // there should be a way to find out which button in the sequence was clicked
     // the programatically select the correct scene from the list of scenes in build
     // rather than 10 functions / a switch with 10 statements
+
+    // loadscene works with indices
+
+    public void LoadLevel(Button button)
+    {
+        SceneManager.LoadScene(button.name);
+    }
 
     public void QuitToTitle()
     {
