2817f86a32af2114d8dfd324b1e01152c3ce8fa1
Dion Pimentel <dionpimentel260@gmail.com>
2021-01-23 11:44:58 -0700

    new titlescreen

---
+++ Assets/TitleBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleBall : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector3(2.5f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
